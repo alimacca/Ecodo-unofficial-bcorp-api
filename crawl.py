6e41dcd8091d21cdebd50e61194a446527996809
alimacca <ali@distinctsignal.com>
2024-07-08 12:24:55 +0200

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -1,6 +1,6 @@
 import scrapy
-from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.contrib.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
 class BCorpItem(scrapy.Item):
     name = scrapy.Field()
@@ -10,22 +10,15 @@ class BCorpItem(scrapy.Item):
     location = scrapy.Field()
     description = scrapy.Field()
 
-
 class BCorpSpider(CrawlSpider):
     name = 'bcorp'
     allowed_domains = ['bcorporation.net']
-    start_urls = ['https://www.bcorporation.net/community/find-a-b-corp?page=1']
-    rules = (Rule(LinkExtractor(allow=['/community/'], deny=['find-a-b-corp']), 'parse_bcorp'),
-            Rule(LinkExtractor(allow=['/community/find-a-b-corp']), follow=True))
+    start_urls = ['https://www.bcorporation.net/en-us/find-a-b-corp']
+
+    rules = (
+        Rule(LinkExtractor(allow=('/en-us/find-a-b-corp/')), callback='parse_bcorp', follow=True),
+        Rule(LinkExtractor(allow=('/en-us/find-a-b-corp/page/\d+')), follow=True)
+    )
 
     def parse_bcorp(self, response):
         bcorp = BCorpItem()
-        bcorp['url'] = response.url
-        bcorp['name'] = response.xpath('//*[@id="page-title"]/text()').extract()[0].strip()
-        bcorp['website'] = response.xpath('//*[contains(concat( " ", @class, " "  ), concat( " ", "company-desc-inner", " "  ))]//a/text()').extract()[0].strip()
-        bcorp['bscore'] = int(response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "field-name-field-overall-b-score", " " ))]//*[contains(concat( " ", @class, " " ), concat( " ", "even", " " ))]/text()').extract()[0].strip())
-        temp_desc = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "field-label-hidden", " " ))]//p/text()').extract()
-        if temp_desc:
-            bcorp['description'] = temp_desc[0].strip()
-
-        return bcorp
